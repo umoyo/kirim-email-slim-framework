f8721ce7e7301435b8094f27b384a4c2d7470473
umoyo <51979880+umoyo@users.noreply.github.com>
2019-06-19 18:30:48 +0700

    Update routes.php
    variabel bahasa indonesia

--- routes.php
+++ routes.php
@@ -9,7 +9,7 @@ return function (App $app)
 
 {
     $container = $app->getContainer();
-/*
+    /*
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
@@ -17,28 +17,26 @@ return function (App $app)
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
-*/
+    */
 
-$transport = new \Swift_SmtpTransport('smtp.mailtrap.io', 2525, '');
-$transport->setUsername('9fa2c9138f6699')->setPassword('ce78083430a853');
+ $masuk_email = new \Swift_SmtpTransport('smtp.mailtrap.io', 2525, '');
+ $masuk_email ->setUsername('9fa2c9138f6699')->setPassword('ce78083430a853');
 
-$app->post("/kirimEmail",function(Request $request, Response $response) use ($app,$transport) 
-{
-$new_email = $request->getParsedBody();
-$mailer = new \Swift_Mailer($transport);
-// Create a message
-$message = new \Swift_Message($new_email["subjek"]);
-$message ->setFrom(array($new_email["email_saya"] => 'saya'))
-         ->setTo(array($new_email["email_kamu"] => 'kekamu'))
-         ->setBody($new_email["isi_email"])
-         ->setContentType("text/html");
-
-// Send the message
-if ($mailer->send($message))
-  {echo "email terkirim\n";}
-else
- {echo "gagal dikirim\n";}
-
-});
+ $app->post("/kirimEmail",function(Request $request, Response $response) use ($app,$masuk_email)
+ { 
+    $sudah_masuk = new \Swift_Mailer($masuk_email);
+    // membuat email baru
+    $ambil_data = $request->getParsedBody();
+    $surat = new \Swift_Message($ambil_data["subjek"]);
+    $surat  ->setFrom(array($ambil_data["email_saya"] => 'saya'))
+            ->setTo(array($ambil_data["email_kamu"] => 'kekamu'))
+            ->setBody($ambil_data["isi_email"])
+            ->setContentType("text/html");
+    // mengirimkan email
+    if ($sudah_masuk->send($surat))
+        {echo "email terkirim\n";}
+    else
+        {echo "gagal dikirim\n";}
+ });
 
 };
